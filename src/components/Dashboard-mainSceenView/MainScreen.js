793fa2c526669b4b0cd9fbaf25ba8cb68ef3a942
varsha-rani1212 <varshaoffical1212@gmail.com>
2022-03-14 00:05:33 +0530

    playwith reactVideo player & manually put firebase url

--- src/components/Dashboard-mainSceenView/MainScreen.js
+++ src/components/Dashboard-mainSceenView/MainScreen.js
@@ -1,5 +1,6 @@
 import styles from "./MainScreen.module.css";
 import Carousel from "react-elastic-carousel";
+import ReactPlayer from 'react-player';
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -15,24 +16,19 @@ function MainScreen() {
         <h2>&nbsp;Uploaded Videos</h2>
       <Carousel breakPoints={breakPoints}>
         <div className={styles.item}>
-        <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+        <ReactPlayer controls url='https://firebasestorage.googleapis.com/v0/b/loginproject-28b6c.appspot.com/o/files%2FWhatsApp%20Video%202022-03-07%20at%205.42.45%20PM.mp4?alt=media&token=3f9c98b2-f31d-4727-8dc7-567f9cf4b340' className={styles.reactPlayer} />
         </div>
         <div className={styles.item}>
-        <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+        <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' className={styles.reactPlayer} />
         </div>
         <div className={styles.item}>
-        <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+        <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' className={styles.reactPlayer} />
         </div>
         <div className={styles.item}>
-        <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+        <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' className={styles.reactPlayer} />
         </div>
         <div className={styles.item}>
-           <iframe src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
+        <ReactPlayer controls url='https://www.youtube.com/watch?v=ysz5S6PUM-U' className={styles.reactPlayer} />
         </div>
       </Carousel>
     </div>
